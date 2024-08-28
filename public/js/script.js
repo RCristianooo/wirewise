d18bf982b7dd53673d84b45673cd73613b0c9c98
Cristian <r.cristiano.ai@gmail.com>
2024-08-28 16:38:32 -0400

    Modified js script as well as page

---
+++ public/js/script.js
@@ -0,0 +1,30 @@
+let sendBtn = document.getElementById('sendBtn');
+let textbox = document.getElementById('textbox');
+let chatContainer = document.getElementById('chatContainer');
+let user = {message:""};
+
+
+function sendMsg(userMsg){
+    let msgElement = document.createElement('div');
+    msgElement.style.textAlign = "right";
+    msgElement.style.margin = "10px"
+
+    msgElement.innerHTML = "<span> You: </span>"+
+                          "<span>" +userMsg+ "</span>";
+
+    chatContainer.appendChild(msgElement);
+}
+
+
+sendBtn.addEventListener('click', function(e){
+    let userMsg = textbox.value;
+    
+    if(userMsg == ""){
+        alert('Please type in a message');
+    } else {
+        let userMsgTxt = userMsg.trim();
+        user.message = userMsgTxt;
+        textbox.value = "";
+        sendMsg(userMsgTxt);
+    }
+})
