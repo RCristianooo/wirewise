04a4bbe460c85e1c1ea6a79751b10d22843e55cc
Cristian <r.cristiano.ai@gmail.com>
2024-09-05 01:30:32 -0400

    Added additional code for chat.js in controllers folder as well more code for chat.js in routes folder.

--- routes/chat.js
+++ routes/chat.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { isAuth } from '../middlewares/isAuth.js';
-import { addConversation, createChat, getAllChats } from '../controllers/chat.js';
+import { addConversation, createChat, deleteChat, getAllChats, getConversation } from '../controllers/chat.js';
 
 const router = express.Router();
 
 router.post("/new", isAuth, createChat);
-router.get("/all", isAuth, getAllChats)
-router.post("/:id", isAuth, addConversation)
+router.get("/all", isAuth, getAllChats);
+router.post("/:id", isAuth, addConversation);
+router.get("/:id", isAuth, getConversation);
+router.delete("/:id", isAuth, deleteChat);
 
 export default router;
