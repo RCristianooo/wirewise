cb390591dc539b51ab35105a4c5c4f7439ba9c36
Cristian <r.cristiano.ai@gmail.com>
2024-09-04 23:05:41 -0400

    Created server.js, installed several dependencies, created a config, controllers, middlewares, models, and routes folder as well as several new files and code.

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import dotenv from 'dotenv'
+import connectDB from './config/database.js';
+
+// const express = require("express");
+// const ejs = require("ejs");
+
+dotenv.config({ path: "./config/.env" })
+
+const app = express();
+
+//importing routes
+import userRoutes from "./routes/user.js"
+
+//using routes
+app.use("/api/user", userRoutes)
+
+// app.use(express.static("public"));
+
+// //set view engine to ejs
+// app.set('view engine', 'ejs');
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}, you better catch it!`);
+    connectDB();
+});
+
+// app.get('/', function(req, res){
+//     res.render('pages/index');
+// });
+
+// app.get('/aboutus', function(req, res){
+//     res.render('pages/aboutus');
+// })
+
+// app.get('/contact', function(req, res){
+//     res.render('pages/contact');
+// })
